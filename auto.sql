9bd5f5701f6570ebdca02dd889c5570843075c31
AleksandrSultanov <57105201+AleksandrSultanov@users.noreply.github.com>
2019-12-04 00:50:37 +0400

    db_auto

--- auto.sql
+++ auto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Дек 03 2019 г., 10:11
+-- Время создания: Дек 03 2019 г., 21:46
 -- Версия сервера: 10.3.16-MariaDB
 -- Версия PHP: 7.3.6
 
@@ -43,12 +43,10 @@ CREATE TABLE `car` (
 --
 
 INSERT INTO `car` (`id_car`, `mark`, `model`, `production_year`, `cost`, `mileage`, `file_path`) VALUES
-(37, 'Tesla', 'Model_3', 2019, 3000000, 70, 'user_file/Afavicon (1).png'),
-(40, 'Lada', 'Granta', 2016, 300000, 57000, 'user_file/Camry_1.png'),
-(49, 'Tesla', 'Roadster', 2019, 10000000, 90, 'user_file/lada.jpg'),
-(50, 'VW', 'Polo', 2018, 800000, 345, 'user_file/renault.jpg'),
-(53, 'Toyota', 'Camry', 2016, 1700000, 110000, 'user_file/nissan.jpg'),
-(58, 'Tesla', 'polo', 2000, 500000, 2000, 'user_file/Afavicon (3).png');
+(37, 'Tesla', 'Model_3', 2019, 3000000, 70, 'user_file/creta.png'),
+(40, 'Lada', 'Granta', 2016, 300000, 57000, 'user_file/Solaris.png'),
+(50, 'VW', 'Polo', 2018, 800000, 345, 'user_file/polo1_(1).png'),
+(53, 'Toyota', 'Camry', 2016, 1700000, 110000, 'user_file/Camry.png');
 
 -- --------------------------------------------------------
 
@@ -69,10 +67,8 @@ CREATE TABLE `relation` (
 INSERT INTO `relation` (`id`, `id_salon`, `id_car`) VALUES
 (9, 1, 37),
 (12, 2, 40),
-(22, 1, 49),
 (23, 3, 50),
-(26, 54, 53),
-(31, 1, 58);
+(26, 54, 53);
 
 -- --------------------------------------------------------
 
@@ -94,30 +90,9 @@ CREATE TABLE `salon` (
 
 INSERT INTO `salon` (`id_salon`, `mark`, `number`, `email`, `file_path`) VALUES
 (1, 'Tesla', '+79044248020', 'email@gmail.como', 'user_file/Afavicon.png'),
-(2, 'Lada', '+79370815100', 'lada@ya.ru', 'user_file/Afavicon_1.png'),
-(3, 'VW', '+79370815122', 'alexul603@gmail.com', 'user_file/creta.png'),
-(54, 'Toyota', '+79370815000', 'alexul603@gmail.ru', 'user_file/kia.jpg');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `secret_information`
---
-
-CREATE TABLE `secret_information` (
-  `id` int(11) NOT NULL,
-  `email` varchar(20) DEFAULT NULL,
-  `number` bigint(11) NOT NULL DEFAULT 0,
-  `phone` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `secret_information`
---
-
-INSERT INTO `secret_information` (`id`, `email`, `number`, `phone`) VALUES
-(0, 'alexul603@gmail.com', 89044248029, 'OnePlus 6'),
-(1, 'alrez@mail.com', 89044248037, 'XIAOMI Mi A1');
+(2, 'Lada', '+79370815100', 'lada@ya.ru', 'user_file/lada.jpg'),
+(3, 'VW', '+79370815122', 'alexul603@gmail.com', 'user_file/polo1.png'),
+(54, 'Toyota', '+79370815000', 'alexul603@gmail.ru', 'user_file/toyotainf.jpg');
 
 --
 -- Индексы сохранённых таблиц
@@ -145,12 +120,6 @@ ALTER TABLE `salon`
   ADD UNIQUE KEY `id_salon` (`id_salon`);
 
 --
--- Индексы таблицы `secret_information`
---
-ALTER TABLE `secret_information`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -170,7 +139,7 @@ ALTER TABLE `relation`
 -- AUTO_INCREMENT для таблицы `salon`
 --
 ALTER TABLE `salon`
-  MODIFY `id_salon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id_salon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
